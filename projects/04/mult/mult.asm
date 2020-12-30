208e42536aa7d571f02e9559607006323101bc2d
Norberts Jegers <norberts.jegers@gmail.com>
2020-12-30 19:14:23 +0200

    implemented mult.asm

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,75 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// PSEUDO __start__
+// a = R0
+// b = R1
+// R2 = 0
+// i = 0
+
+// if (a === 0 || b === 0) {
+//     return 0;
+// }
+
+// while (i < b) {
+//     R2 = R2 + a;
+//     i++:
+// }
+
+// return R2;
+// PSEUDO __end__
+
+// a = R0
+@R0
+D=M
+@a
+M=D
+
+// b = R1
+@R1
+D=M
+@b
+M=D
+
+// R2 = 0
+@R2
+M=0
+
+// i = 0
+@i
+M=0
+
+// if a === 0; return 0
+@a
+D=M
+@END
+D;JEQ
+
+// if b === 0; return 0
+@b
+D=M
+@END
+D;JEQ
+
+(LOOP)
+    @i
+    D=M
+    @b
+    D=D-M // while(i < b) ==> i - b >= 0
+    @END
+    D;JGE
+
+    @a
+    D=M
+    @R2
+    M=M+D // R2 = R2 + a
+
+    @i
+    M=M+1 // i = i + 1
+
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0; JMP
