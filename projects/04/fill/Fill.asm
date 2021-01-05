86a970ad2d3e9a37e10075c83a2761714c0cc488
Norberts Jegers <norberts.jegers@gmail.com>
2021-01-05 23:01:59 +0200

    fill.asm wip

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,44 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// PSUEDO __start__
+
+// const SCREEN = 16384;
+// const ROW_SIZE = 32; // 1 row = 32 words (16-bit)
+// const ROW_COUNT = 256;
+
+// const KBD = 24576; // 1 word
+// let filled = false;
+
+// function writeScreen(value) {
+//     for (let row = 0; row < ROW_COUNT; row++) {
+//         for (let word = 0; word < ROW_SIZE; word++) {
+//             const addr = SCREEN + ROW_SIZE*row + word;
+//             *addr = value;
+//         }
+//     }
+// }
+
+// function fillScreen() {
+//     writeScreen(1);
+//     filled = true;
+// }
+
+// function unfillScreen() {
+//     writeScreen(0);
+//     filled = false;
+// }
+
+// while(true) {
+//     if (KBD > 0 && !filled) {
+//         fillScreen();
+//         continue;
+//     }
+    
+//     if (KBD === 0 && filled) {
+//         unfillScreen();
+//         continue;
+//     }
+// }
+
+// PSEUDO __end__
