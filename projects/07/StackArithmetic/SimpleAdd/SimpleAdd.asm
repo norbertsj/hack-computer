4aca46e26527609155adc83e7008289f1dbd5d20
Norberts Jegers <norberts.jegers@gmail.com>
2021-04-13 23:07:47 +0300

    finished project 7

---
+++ projects/07/StackArithmetic/SimpleAdd/SimpleAdd.asm
@@ -0,0 +1,37 @@
+// push constant 7
+@7
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// push constant 8
+@8
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// add
+@SP
+M=M-1
+A=M
+D=M
+@R15
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@R15
+D=D+M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
